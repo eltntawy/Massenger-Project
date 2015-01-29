6ab0c36812d37b19d1fa81095849385df91531da
Mohamed Refaat <eltntawy@gmail.com>
2015-01-29 13:00:34 +0200

    bug fix for closing server while have online contact

--- Messenger Server Project/src/com/chat/view/ServerMainFrame.java
+++ Messenger Server Project/src/com/chat/view/ServerMainFrame.java
@@ -7,15 +7,21 @@ package com.chat.view;
 
 import java.io.IOException;
 import java.net.URL;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
-import javafx.event.WeakEventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+
+import javax.swing.JOptionPane;
+
+import com.chat.controller.ServerController;
 
 /**
  *
@@ -25,28 +31,39 @@ public class ServerMainFrame extends Application {
 
     @Override
     public void start(final Stage primaryStage) {
-        Parent root = null;
-        try {
-            URL url = getClass().getResource("FXMLMainServerFrame.fxml");
-            root = FXMLLoader.load(url);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        Scene firstScene = new Scene(root);
-        primaryStage.setResizable(false);
-        primaryStage.setScene(firstScene);
-        primaryStage.show();
-        //primaryStage.setFullScreen(true);
+	Parent root = null;
+	try {
+	    URL url = getClass().getResource("FXMLMainServerFrame.fxml");
+	    root = FXMLLoader.load(url);
+	} catch (IOException ex) {
+	    ex.printStackTrace();
+	}
+	Scene firstScene = new Scene(root);
+	primaryStage.setResizable(false);
+	primaryStage.setScene(firstScene);
+	primaryStage.show();
+	// primaryStage.setFullScreen(true);
+	
+	primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+	    @Override
+	    public void handle(WindowEvent event) {
+		if (ServerController.getChatClientVector().size() > 0) {
+		    event.consume();
+		    Alert alert = new Alert(AlertType.ERROR);
+		    alert.setTitle("Error");
+		    alert.setHeaderText("Server closing warning");
+		    alert.setContentText("Please stop server first !!");
 
-            @Override
-            public void handle(WindowEvent event) {
-                primaryStage.close();
-                Platform.exit();
-                System.exit(0);
-            }
+		    alert.showAndWait();
+		    //JOptionPane.showMessageDialog(null, "Please stop server frist");
+		} else {
+		    primaryStage.close();
+		    Platform.exit();
+		    System.exit(0);
+		}
+	    }
 
-        });
+	});
     }
 }
