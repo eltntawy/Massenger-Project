dee0def242274085ba1c7e7508d6e28bf613fd22
Marwa Ibraheem <marwa>
2015-01-30 14:04:29 +0200

    XML
    Merge origin/master
    
    Conflicts:
    	Messenger Client Project/nbproject/private/private.properties
    	Messenger Server Project/nbproject/private/private.properties

--- Messenger Client Project/src/com/xml/XMLJAXB.java
+++ Messenger Client Project/src/com/xml/XMLJAXB.java
@@ -5,9 +5,12 @@
  */
 package com.xml;
 
+import com.chat.model.Message;
 import com.xml.MessageHistory;
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
@@ -31,14 +34,33 @@ public class XMLJAXB {
             MessageHistory element = (MessageHistory) unmarsh.unmarshal(xmlFile);
             System.out.println("marwaaa");
             ObjectFactory factory = new ObjectFactory();
-            MsgBody msg = factory.createMsgBody();
-            msg.setText("Hello");
+            MessageHistory msg = factory.createMessageHistory();
+            Login log=new Login();
+            log.setUserID(0);
+            log.setUserName("Marwa");
+            UserTo user=new UserTo();
+            user.setUserID(1);
+            user.setUserName("aya");
+            MsgBody body=new MsgBody();
+            body.setText("Hello My Friend");
+            user.setBody(body);
+            List<UserTo>userList=new ArrayList<>();
+            List<Login>logList=new ArrayList<>();
+            userList.add(user);
+            logList.add(log);
+           log.setUserTo(userList);
+           msg.setLogin(logList);
             Marshaller marsh = context.createMarshaller();
-            marsh.marshal(element, (new File("Msg.xml")));
+            marsh.marshal(msg, (new File("Msg.xml")));
         } catch (JAXBException ex) {
-           // ex.printStackTrace();
+            //ex.printStackTrace();
             Logger.getLogger(XMLJAXB.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    public void appendMsg(Message message){
+        
+        
     }
 
     public static void main(String[] args) {
